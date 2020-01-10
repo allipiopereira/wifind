163058b860b1b6701d1e5df11b5051fa9a2a3d10
alipiopereira <allipiopereira@gmail.com>
2020-01-10 20:26:24 -0200

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-content>
+        <Presentation/>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  import Presentation from './components/app/Presentation'
+  export default {
+    name: "App",
+    components: {
+      Presentation
+    }
+  };
+</script>
+
+<style>
+  #app {
+    font-family: "Varela Round", sans-serif;
+  }
+
+  html {
+    overflow: hidden;
+  }
+
+  .active {
+    border-bottom: 3px solid #0AE6B2;
+  }
+
+  .btn-shadow {
+    box-shadow: 0px 2px 12px rgba(100, 100, 100, 0.2);
+    margin: 5px;
+  }
+
+  .shadow {
+    box-shadow: 0px 2px 12px rgba(100, 100, 100, 0.2);
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import Typer from "vue-typer";
+import App from "./App.vue";
+import "vuetify/dist/vuetify.css";
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
+Vue.use(Typer);
+
+new Vue({
+  vuetify: new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          perfect: "#fc4850",
+          primary: "#3f51b5",
+          secondary: "#b0bec5",
+          accent: "#8c9eff",
+          error: "#b71c1c"
+        }
+      }
+    }
+  }),
+  render: h => h(App)
+}).$mount("#app");
